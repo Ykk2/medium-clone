c951c4acca2e723107d4cf823424d80a1bb51cbd
Andrew <andrewkim1998@gmail.com>
2022-12-10 15:00:09 -0600

    story store base

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import story from './story'
 
 const rootReducer = combineReducers({
-  session,
+  session, story
 });
 
 
