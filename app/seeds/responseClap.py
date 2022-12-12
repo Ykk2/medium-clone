6bbcb77c4de4e00dd8654d96cac4c8ba18d1b9a3
DLiaw <DL0429922@gmail.com>
2022-12-12 01:56:48 -0500

    fixed seeder and maybe cascade

--- app/seeds/responseClap.py
+++ app/seeds/responseClap.py
@@ -26,12 +26,72 @@ def seed_ResponseClaps():
         userId='3',
         responseId='5',
     )
+    responseClap6 = ResponseClap(
+        id='6',
+        userId='8',
+        responseId='6',
+    )
+    responseClap7 = ResponseClap(
+        id='7',
+        userId='2',
+        responseId='4',
+    )
+    responseClap8 = ResponseClap(
+        id='8',
+        userId='3',
+        responseId='8',
+    )
+    responseClap9 = ResponseClap(
+        id='9',
+        userId='4',
+        responseId='5',
+    )
+    responseClap10 = ResponseClap(
+        id='10',
+        userId='6',
+        responseId='6',
+    )
+    responseClap11 = ResponseClap(
+        id='11',
+        userId='7',
+        responseId='2',
+    )
+    responseClap12 = ResponseClap(
+        id='12',
+        userId='2',
+        responseId='1',
+    )
+    responseClap13 = ResponseClap(
+        id='13',
+        userId='8',
+        responseId='3',
+    )
+    responseClap14 = ResponseClap(
+        id='14',
+        userId='6',
+        responseId='6',
+    )
+    responseClap15 = ResponseClap(
+        id='15',
+        userId='4',
+        responseId='8',
+    )
 
     db.session.add(responseClap1)
     db.session.add(responseClap2)
     db.session.add(responseClap3)
     db.session.add(responseClap4)
     db.session.add(responseClap5)
+    db.session.add(responseClap6)
+    db.session.add(responseClap7)
+    db.session.add(responseClap8)
+    db.session.add(responseClap9)
+    db.session.add(responseClap10)
+    db.session.add(responseClap11)
+    db.session.add(responseClap12)
+    db.session.add(responseClap13)
+    db.session.add(responseClap14)
+    db.session.add(responseClap15)
     db.session.commit()
 
 def undo_responseClaps():
