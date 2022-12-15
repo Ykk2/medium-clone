fde2e7c403be677327a143dc176741234640310c
Richard <richardkwon5@gmail.com>
2022-12-14 16:40:00 -0800

    pausing

---
+++ react-app/src/components/MyStories/FollowingButton.js
@@ -0,0 +1,36 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink, useHistory } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { gettingFollows, addingFollow, deletingFollow } from '../../store/follow';
+
+
+function FollowingButton({followerId}) {
+
+    const dispatch = useDispatch()
+
+    const [follow, setFollowing] = useState(true)
+
+    const handleFollowClick = (e) => {
+        e.preventDefault()
+        dispatch(addingFollow(followerId))
+        setFollowing(true)
+    }
+
+    const handleRemoveFollowClick = (e) => {
+        e.preventDefault()
+        dispatch(deletingFollow(followerId))
+        setFollowing(false)
+    }
+
+    return (
+        <>
+            {follow ?
+            <button onClick={handleRemoveFollowClick}>Unfollow</button>
+            :
+            <button onClick={handleFollowClick}>Follow</button>
+            }
+        </>
+    )
+}
+
+export default FollowingButton
